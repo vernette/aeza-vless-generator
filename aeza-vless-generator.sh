6f32907eb30d7087006c416c093d64e05820f1eb
Nikita Skryabin <nekit2271@gmail.com>
2024-11-07 01:42:04 +0300

    feat(aeza-vless-generator.sh): add functionality to retrieve a temporary email using an API endpoint and implement basic script structure

--- aeza-vless-generator.sh
+++ aeza-vless-generator.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+EMAIL_API_ENDPOINT="https://api.internal.temp-mail.io/api/v3/email"
+
+get_email() {
+  printf "%s" "$(curl -sX POST "$EMAIL_API_ENDPOINT/new" | jq -r '.email')"
+}
+
+main() {
+  email=$(get_email)
+}
+
+main
