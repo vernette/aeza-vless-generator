a9cc07baefccddcdfd9baa24db2939af675bc718
Nikita Skryabin <nekit2271@gmail.com>
2024-11-10 21:21:53 +0300

    fix(aeza-vless-generator.sh): improve user experience by adding a descriptive message while waiting for the random sleep time

--- aeza-vless-generator.sh
+++ aeza-vless-generator.sh
@@ -251,7 +251,7 @@ get_api_token() {
   response=$(curl_request "$AEZA_API_ENDPOINT/auth-confirm" "POST" --user-agent "$USER_AGENT" --header "Device-Id: $device_id" --data "{\"email\": \"$email\", \"code\": \"$code\"}")
   api_token=$(process_json "$response" '.response.token')
   log_message "INFO" "API token: $api_token"
-  log_message "INFO" "Sleeping for a random amount of time (from $min_sleep_time to $max_sleep_time secs)"
+  log_message "INFO" "Sleeping for a random amount of time (from $min_sleep_time to $max_sleep_time secs), please wait..."
   sleep $((RANDOM % (max_sleep_time - min_sleep_time + 1) + min_sleep_time))
 }
 
